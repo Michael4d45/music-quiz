c19e694b8e30c110763ea9eec528bdf0a1f9efe9
Michael Harvey <michael4d45@gmail.com>
2025-12-24 12:12:02 -0700

    frontend lint

--- resources/js/types/generated.d.ts
+++ resources/js/types/generated.d.ts
@@ -1,24 +1,24 @@
 declare namespace Models {
-export type UserData = {
-id: number;
-name: string;
-is_admin: boolean;
-email: string;
-};
+    export type UserData = {
+        id: number;
+        name: string;
+        is_admin: boolean;
+        email: string;
+    };
 }
 declare namespace Props {
-export type AuthProps = {
-user: Models.UserData | null;
-impersonator: Models.UserData | null;
-};
-export type FlashProps = {
-success?: string | boolean | null;
-error?: string | null;
-info?: string | null;
-warning?: string | null;
-};
-export type SharedProps = {
-auth: Props.AuthProps;
-flash: Props.FlashProps;
-};
+    export type AuthProps = {
+        user: Models.UserData | null;
+        impersonator: Models.UserData | null;
+    };
+    export type FlashProps = {
+        success?: string | boolean | null;
+        error?: string | null;
+        info?: string | null;
+        warning?: string | null;
+    };
+    export type SharedProps = {
+        auth: Props.AuthProps;
+        flash: Props.FlashProps;
+    };
 }
